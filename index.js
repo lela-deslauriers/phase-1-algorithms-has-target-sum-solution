1cef3e7cab1f364031861e1a0b4107dea6b5231c
Lela Deslauriers <lela.deslauriers@gmail.com>
2023-01-07 22:22:00 -0800

    commit tree 2ff0ad6ab3485ce4b0bd4408a13543145c0dd9c1

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    console.log(`seenNumbers = ${seenNumbers}`);
+    console.log(`i = ${i}`)
+    console.log(`complement = ${target - array[i]}`);
+    if (seenNumbers[complement]) return true;
+    seenNumbers[array[i]] = true;
+    console.log(`seenNumbers assignment = ${seenNumbers}`);
+  }
+  return false;
 }
 
 /* 
@@ -8,6 +19,14 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  create an object to save numbers we've already seen
+  iterate through each number in an array
+  for the current number, identify a complement that adds to the target (comp = target - num)
+  check if any key on our object is the complement
+
+  if so return true
+  otherwise add that number to the object
+  if I reach the end of the array return falsse
 */
 
 /*
